6f2dd103500e9d678a6217be652a8e21d9a7a00b
Mok Zheng Jue <zheng.jue@live.com>
2017-01-12 00:40:55 +0800

    plot 1

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ source("read.R") # read in both data sets
 png(filename="plot1.png")
 
 #Total up emissions by year and assign to 'y'.
-tapply(NEI$Emissions,NEI$year,sum) 
+y<-tapply(NEI$Emissions,NEI$year,sum) 
 
 #plot y.
 barplot(y,main="PM25 emissions over the years",xlab="Year",ylab="PM25 emission")
