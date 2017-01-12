10baedb25ecf2d60c03f400733acdb2f1501fcb0
JoshuaMZJ <zheng.jue@live.com>
2017-01-12 17:14:04 +0800

    plot 6

---
+++ plot6.R
@@ -0,0 +1,23 @@
+#check if ggplot is installed
+if(!require("ggplot2")){install.packages(ggplot2)}
+library(ggplot2)
+
+#subset for data set containing only Baltimore
+x<-subset(NEI,NEI$fips=="24510"|NEI$fips=="06037")
+x$year<-factor(x$year)
+
+#Find SCC values for vehicles
+valid.SCC<-SCC[grep(".*vehicle.*",SCC[,8],ignore.case=TRUE),1]
+
+#further subset NEI set for vehicles emission
+x<-x[x$SCC%in%valid.SCC,]
+
+#Convert 'fips' into factor and rename labels.
+x$fips<-factor(x$fips,levels=c("24510","06037"),labels=c("Baltimore City","Los Angeles County"))
+
+png(filename="plot6.png",width=500,height=500)
+
+g<-ggplot(x,aes(year,Emissions))
+print(g+geom_bar(stat="sum",aes(fill=year))+labs(x="Year",y=expression("PM"[2.5]*" emissions"),title="Baltimore and LA PM2.5 emissions from vehicle sources")+theme(legend.position="none")+facet_grid(.~fips))
+
+dev.off()
